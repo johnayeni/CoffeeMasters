65b030cd8cf6dddcc278cf1f04388d325aa2bf17
John Ayeni <johnayeni10.aj@gmail.com>
2023-03-25 16:37:18 +0100

    Initial Commit

---
+++ CoffeeMasters/CoffeeMastersApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeMastersApp.swift
+//  CoffeeMasters
+//
+//  Created by John Ayeni on 3/25/23.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeMastersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
